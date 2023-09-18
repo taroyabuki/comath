0925616a1538e2c87899baa1d5130399474407d9
Taro Yabuki <taroyabuki@users.noreply.github.com>
2023-09-18 16:26:01 +0900

    mathematica

--- code/createnb.m
+++ code/createnb.m
@@ -1,18 +1,2 @@
-nb = EvaluationNotebook[];
-
-url = "https://raw.githubusercontent.com/taroyabuki/comath/main/code/mathematica.md";
-
-md = Drop[Import[url, {"Text", "Lines"}], 6];
-
-in = False; cell = "";
-Do[
- If[And[StringLength[line] > 1, StringTake[line, 1] == "#"],
-  NotebookWrite[nb, Cell[StringDrop[line, 2], "Chapter"]],
-  If[line == "```",
-   in = False;
-   NotebookWrite[nb, Cell[StringDrop[cell, -1], "Input"]],
-   If[line == "```code",
-    in = True;
-    cell = "",
-    If[in, cell = cell <> line <> "\n"]]]],
- {line, md}]
+nbContent = Import["https://raw.githubusercontent.com/taroyabuki/comath/main/code/mathematica.nb", "NB"];
+NotebookWrite[InputNotebook[], nbContent];
